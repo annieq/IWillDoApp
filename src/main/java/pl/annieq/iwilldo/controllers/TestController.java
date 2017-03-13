59878d28a6a779b641f5bb7c49bc53f1a3ecaf8d
ASzoltysek <A@ania.home>
2017-03-13 21:59:58 +0100

    Adding example login page; Reorganizing resources

--- src/main/java/pl/annieq/iwilldo/controllers/TestController.java
+++ src/main/java/pl/annieq/iwilldo/controllers/TestController.java
@@ -1,6 +1,7 @@
 package pl.annieq.iwilldo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,5 +14,11 @@ public class TestController {
         String message = "<br><div style='text-align:center;'>"
                 + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
         return new ModelAndView("welcome", "message", message);
+    }
+    
+    @RequestMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("test", "test message");
+        return "login";
     }
 }
